ed11967f8873cbe3539456b2a21fee98a8753cea
yangjun@chinafintech.cn <yangjun@chinafintech.cn>
2019-09-25 14:17:23 +0800

    增加部分demo

---
+++ src/views/Demo.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="container">
+        <div class="group">
+            <div class="title">支付图标</div>
+            <div class="content">
+                <div style="margin: 0 12px">
+                    <PaymentIcon code="Alipay"/>&nbsp;
+                    <PaymentIcon code="WechatPay"/>&nbsp;
+                    <PaymentIcon code="UnionPay_QR"/>
+                </div>
+            </div>
+        </div>
+
+        <div class="group">
+            <div class="title">Vant输入框</div>
+            <div class="content">
+                <CellGroup>
+                    <Field
+                        required
+                        clearable
+                        label="用户名"
+                        right-icon="question-o"
+                        placeholder="请输入用户名"
+                    />
+                </CellGroup>
+            </div>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+    import {Component, Vue} from 'vue-property-decorator';
+    import {CellGroup, Cell, Field} from 'vant';
+    import PaymentIcon from '@/components/PaymentIcon';
+
+    @Component({
+        components: {
+            CellGroup,
+            Cell,
+            Field,
+            PaymentIcon,
+        },
+    })
+    export default class Demo extends Vue {
+    }
+</script>
+<style lang="less" scoped>
+    @import "~@/styles/mixins";
+
+    .group{
+        margin-bottom: 12px;
+    }
+    .title{
+        padding: 12px;
+        border-top: solid 1px @border-color;
+    }
+    .content{
+        padding: 12px 0;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,36 +4,60 @@
             <img alt="logo" src="../assets/logo.png" class="logo">
             <h1>Papaw !</h1>
         </div>
-        <router-link to="about">组件示例</router-link>
-        <PaymentIcon code="Alipay"/>
-        <PaymentIcon code="Alipay22"/>
+        <div class="desc">
+            papaw是一个基于VUE+Vant的前端脚手架，使用vue-cli构建，并增加pwa、环境配置、本地能力模拟等功能
+        </div>
+        <CellGroup>
+            <Cell title="组件示例" is-link to="demo"/>
+            <Cell title="系统配置" @click="showSetting"/>
+            <Cell title="启动参数配置" @click="showStartParams"/>
+        </CellGroup>
     </div>
 </template>
 
 <script lang="ts">
     import {Component, Vue} from 'vue-property-decorator';
-    import PaymentIcon from '@/components/PaymentIcon.vue'; // @ is an alias to /src
+    import {CellGroup, Cell, Dialog} from 'vant';
 
     @Component({
         components: {
-            PaymentIcon,
+            CellGroup,
+            Cell
         },
     })
     export default class Home extends Vue {
+        showSetting = () => {
+            Dialog({title: '设置', message: JSON.stringify(Settings)});
+        };
+        showStartParams = () => {
+            Dialog({title: '启动参数', message: JSON.stringify(StartParams)});
+        }
     }
 </script>
 
 <style lang="less" scoped>
-    .container{
-        padding: 24px;
+    @import "~@/styles/mixins";
+
+    .container {
+        padding: 24px 0;
     }
-    .header{
+
+    .header {
         text-align: center;
-        .logo{
+
+        .logo {
             width: 80px;
         }
-        h1{
+
+        h1 {
             font-size: 20px;
         }
+    }
+
+    .desc {
+        padding: 24px;
+        font-size: 14px;
+        background: @white;
+        margin: 12px 0;
     }
 </style>
