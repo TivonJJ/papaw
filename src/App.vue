8470c49dde33aefb68c1985d3d4def91e6d2a0c4
yangjun@chinafintech.cn <yangjun@chinafintech.cn>
2019-12-23 14:23:31 +0800

    增加页面切换动画

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
     <div id="app">
-        <router-view />
+        <transition :name="transitionName">
+            <router-view />
+        </transition>
     </div>
 </template>
 <script>
@@ -13,8 +15,50 @@ Vue.use(Dialog)
 
 export default {
     name: 'App',
+    data() {
+        return {
+            transitionName: '',
+        };
+    },
+    watch: {
+        //使用watch 监听$router的变化
+        $route(to, from) {
+            const toPath = to.path.split('/').filter(n => !!n);
+            const fromPath = from.path.split('/').filter(n => !!n);
+            this.transitionName =
+                toPath.length >= fromPath.length ? 'slide-left' : 'slide-right';
+        },
+    },
 };
 </script>
 <style lang="less">
 @import '~@/styles/common';
+#app > div {
+    min-width: 100vw;
+    min-height: 100vh;
+}
+.slide-right-enter-active,
+.slide-right-leave-active,
+.slide-left-enter-active,
+.slide-left-leave-active {
+    will-change: transform;
+    transition: all 500ms;
+    position: absolute;
+}
+.slide-right-enter {
+    opacity: 0;
+    transform: translate3d(-100%, 0, 0);
+}
+.slide-right-leave-active {
+    opacity: 0;
+    transform: translate3d(100%, 0, 0);
+}
+.slide-left-enter {
+    opacity: 0;
+    transform: translate3d(100%, 0, 0);
+}
+.slide-left-leave-active {
+    opacity: 0;
+    transform: translate3d(-100%, 0, 0);
+}
 </style>
